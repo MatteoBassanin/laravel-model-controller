f346f871883d9edb1279bae66a239b87eb1fcc1f
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-11 16:09:34 +0200

    printing movies list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>home</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+@section('content')
+    @foreach ($movies as $movie)
+        <div class="ms_container">
+            <h2 class="title">{{ $movie->title }}</h2>
+            <h3 class="original_title">{{ $movie->original_title }}</h3>
+            <h4 class="nationality">{{ $movie->nationality }} </h4>
+            <h5 class="date">{{ $movie->date }}</h5>
+            <h6 class="vote">{{ $movie->vote }}</h6>
+        </div>
+    @endforeach
+@endsection
